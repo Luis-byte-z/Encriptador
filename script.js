755bd5d4c559e97ad07846f5809a87a4bccb906b
hayyrs66 <rayrtsx@proton.me>
2024-04-01 16:44:01 -0600

    Update styles and functionality

---
+++ script.js
@@ -0,0 +1,6 @@
+
+const cajaCopiar = document.querySelector('.caja-copiar');
+const altura = document.getElementById('textarea-texto').offsetHeight + "px";
+console.log(altura)
+cajaCopiar.style.height = altura;
+
